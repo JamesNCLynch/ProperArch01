ea0650276bdb1e57868f74dfa7e482424647766d
jlynch <jlynch@actionpoint.ie>
2019-06-17 17:45:25 +0100

    Reworked how the navbar menu links are stored. Added sidebar menus to Home pages and some general page housekeeping

--- ProperArch01.WebApp/Helpers/HelperMethods.cs
+++ ProperArch01.WebApp/Helpers/HelperMethods.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ProperArch01.Contracts.Constants;
-using ProperArch01.Contracts.Services;
-using ProperArch01.Contracts.Dto;
+using ProperArch01.WebApp.Models;
 using System.Configuration;
 
 namespace ProperArch01.WebApp.Helpers
@@ -36,6 +35,155 @@ namespace ProperArch01.WebApp.Helpers
             IEnumerable<int> hours = Enumerable.Range(earliestSlotStartHour, latestSlotEndHour - earliestSlotStartHour);
 
             return hours.ToList();
+        }
+
+        // menu lists
+
+        public static List<MenuListItemModel> GetAboutMenu()
+        {
+            return new List<MenuListItemModel>()
+            {
+                new MenuListItemModel
+                {
+                    DisplayName = "Mission/Aim",
+                    ControllerName = "Home",
+                    ActionName = "MissionAim"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "History",
+                    ControllerName = "Home",
+                    ActionName = "History"
+                }
+            };
+        }
+
+        public static List<MenuListItemModel> GetRatesMenu()
+        {
+            return new List<MenuListItemModel>()
+            {
+                new MenuListItemModel
+                {
+                    DisplayName = "Membership Rates",
+                    ControllerName = "Home",
+                    ActionName = "MembershipRates"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Corporate Rates",
+                    ControllerName = "Home",
+                    ActionName = "CorporateRates"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Pay As You Go",
+                    ControllerName = "Home",
+                    ActionName = "Payg"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Room/Facility Rates",
+                    ControllerName = "Home",
+                    ActionName = "RoomFacilityRates"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Booking Forms",
+                    ControllerName = "Home",
+                    ActionName = "BookingForms"
+                }
+            };
+        }
+        public static List<MenuListItemModel> GetFacilitiesMenu()
+        {
+            return new List<MenuListItemModel>()
+            {
+                new MenuListItemModel
+                {
+                    DisplayName = "Sports Hall",
+                    ActionName = "SportsHall",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Gym",
+                    ActionName = "Gym",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Activities Room",
+                    ActionName = "Activities",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "All Weather Pitch",
+                    ActionName = "Pitch",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Meeting Rooms",
+                    ActionName = "MeetingRooms",
+                    ControllerName = "Home"
+                },
+            };
+        }
+        public static List<MenuListItemModel> GetActivitiesMenu()
+        {
+            return new List<MenuListItemModel>()
+            {
+                new MenuListItemModel
+                {
+                    DisplayName = "MLC Class and Timetable",
+                    ActionName = "Index",
+                    ControllerName = "ClassTimetable"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Summer Camps",
+                    ActionName = "SummerCamps",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Birthday Parties",
+                    ActionName = "Birthday",
+                    ControllerName = "Home"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Gallery",
+                    ActionName = "Gallery",
+                    ControllerName = "Home"
+                },
+            };
+        }
+
+        public static List<MenuListItemModel> GetAdminMenu()
+        {
+            return new List<MenuListItemModel>()
+            {
+                new MenuListItemModel
+                {
+                    DisplayName = "Create a class type",
+                    ActionName = "Create",
+                    ControllerName = "ClassType"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Scheduled classes need completion",
+                    ActionName = "Index",
+                    ControllerName = "ScheduledClass"
+                },
+                new MenuListItemModel
+                {
+                    DisplayName = "Add a holiday",
+                    ActionName = "Create",
+                    ControllerName = "Holiday"
+                }
+            };
         }
     }
 }
---
+++ ProperArch01.WebApp/Models/MenuListItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProperArch01.WebApp.Models
+{
+    public class MenuListItemModel
+    {
+        public string DisplayName { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+}
