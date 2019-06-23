5f80e8335843dc126f4cf88759a11f74bf215de4
jlynch <jlynch@actionpoint.ie>
2019-06-23 09:57:21 +0100

    Added migration data seeding for class types

--- ProperArch01.Persistence/Migrations/Configuration.cs
+++ ProperArch01.Persistence/Migrations/Configuration.cs
@@ -23,13 +23,50 @@ namespace ProperArch01.Persistence.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-            SeedRolesAndUsers();
+            SeedRolesAndUsers(context);
+            SeedInitialClassTypes(context);
         }
 
-        private void SeedRolesAndUsers()
+        private void SeedInitialClassTypes(ProperArch01DbContext context)
         {
-            var context = new ProperArch01DbContext();
+            context.ClassTypes.AddOrUpdate(
+                new ClassType
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Barbell Blast",
+                    Difficulty = 80,
+                    IsActive = true,
+                    Description = "This class is for anyone looking to get lean, toned and fit – fast. Using light to moderate weights with lots of repetition, barbell blast gives you a total body workout. You’ll leave the class feeling challenged and motivated, ready to come back for more.",
+                    ImageFileName = "barbell-power-sm.jpg"
+                }
+            );
+            context.ClassTypes.AddOrUpdate(
+                new ClassType
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Rebel Training",
+                    Difficulty = 90,
+                    IsActive = true,
+                    Description = "A high intensity cardio, strength and conditioning class that will push your body to its upper limits. You will get the ultimate body workout, pushing yourself further and performing at a higher intensity. (This class is recommended for those with a good level of fitness).",
+                    ImageFileName = "mlc-rebel-sm.jpg"
+                }
+            );
+            context.ClassTypes.AddOrUpdate(
+                new ClassType
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Fab Abs",
+                    Difficulty = 60,
+                    IsActive = true,
+                    Description = "This is a 30 minute intense workout for your core where you will learn new exercises to help sculpt the body. This class is for all fitness levels and is a great way to fit a quick but challenging workout into your busy day.",
+                    ImageFileName = "mlc-abs.jpg"
+                }
+            );
+            context.SaveChanges();
+        }
 
+        private void SeedRolesAndUsers(ProperArch01DbContext context)
+        {
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             var userManager = new UserManager<GymUser>(new UserStore<GymUser>(context));
 
